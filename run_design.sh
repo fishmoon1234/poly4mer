0e5dfc2e840a988a7aa018c0333004e131903457
naiyu <naiyu.yin@gmail.com>
2026-05-29 18:28:28 -0400

    Rename run_design_v2.sh -> run_design.sh; clean up
    Drop the _v2 suffix on the launcher now that the v1 design script
    is gone; symmetric with run_property_prediction.sh. Also remove
    dead commented-out v1 invocation block and normalize indentation.

---
+++ run_design.sh
@@ -0,0 +1,33 @@
+# tunable hyperparameters
+c_tig=0.1
+c_qpua=1
+c_sea=1
+c_co=1
+max_epochs=500
+flux=75
+thickness=6
+
+echo -e "PERFORMING OPTIMIZATION.\n"
+
+# test multiple initial polymers
+for idx in {1..28};
+do
+    # test with multiple learning rates
+    for lr in {0.005,0.01,0.02,0.05,0.1};
+    do
+        echo "Running idx=$idx with lr=$lr ..."
+        python fire_optimize_smiles_v2.py \
+            --lr_start ${lr} \
+            --max_epochs ${max_epochs} \
+            --c_tig ${c_tig} \
+            --c_qpua ${c_qpua} \
+            --c_sea ${c_sea} \
+            --c_co ${c_co} \
+            --smiles_idx ${idx} \
+            --flux ${flux} \
+            --thickness ${thickness}
+    done
+done
+
+wait
+echo 'ALL runs finished'
